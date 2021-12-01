5392b1898f611ff8664dbdbeafe86ed2cb9f1c1f
Laxmikant Parate <laxmikantparate01@gmail.com>
2021-12-01 23:28:03 +0530

    Addded fixed logo and lifted it top using the zIndex

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import React from "react";
-import { Dimensions, FlatList, StyleSheet } from "react-native";
+import { Dimensions, FlatList, Image, StyleSheet, View } from "react-native";
 
 import CarItem from "./app/components/CarItem";
 import cars from "./app/api/cars";
 
 export default function App() {
   return (
-    <FlatList
-      data={cars}
-      keyExtractor={({ name }) => name}
-      renderItem={({ item }) => <CarItem car={item} />}
-      snapToAlignment={"start"}
-      decelerationRate={"fast"}
-      snapToInterval={Dimensions.get("screen").height}
-    />
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image
+          source={require("./assets/images/logo.png")}
+          style={styles.logo}
+        />
+        <Image
+          source={require("./assets/images/menu.png")}
+          style={styles.menu}
+        />
+      </View>
+      <FlatList
+        data={cars}
+        keyExtractor={({ name }) => name}
+        renderItem={({ item }) => <CarItem car={item} />}
+        snapToAlignment={"start"}
+        decelerationRate={"normal"}
+        snapToInterval={Dimensions.get("screen").height}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  logoContainer: {
+    paddingHorizontal: "7%",
+    width: "100%",
+    top: "7%",
+    position: "absolute",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    zIndex: 100,
+  },
+  logo: {
+    height: 13,
+    width: 100,
+  },
+  menu: {
+    width: 25,
+    height: 25,
+  },
+});
